fdfb30ffc0ddb8fac87ec6450eae6fb68b8a9261
Carlos Filoteo <c.filoteo12@gmail.com>
2023-03-13 23:37:13 -0600

    Remove unused helper

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 // import svgSprites from 'rollup-plugin-svg-sprites'
 import { resolve } from 'path'
 
-// // https://www.geeksforgeeks.org/how-to-convert-a-string-into-kebab-case-using-javascript/
-// const kebabCase = (str) =>
-//   str
-//     .replace(/([a-z])([A-Z])/g, '$1-$2') // replace "camelCase" to "camel-case"
-//     .replace(/[\s_\/]+/g, '-') // Replace spaces, underscore, and slash with - (dash)
-//     .toLowerCase()
-
 export default defineConfig({
   build: {
     lib: {
